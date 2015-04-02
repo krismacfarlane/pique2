af9406bee490d311c92a51c063d2edf8d342bf04
Kamari Aykes <aykeskc@gmail.com>
2015-04-02 16:38:13 -0400

    updates errored seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+User.destroy_all
+# Friendship.destroy_all
+# Friendship_Profiles.destroy_all
+
+
+
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +15,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+sam = User.create([{ name: "Samuel"}])
+@jackson = User.create([{ name: "Jackson"}])
+
+@justin = User.create([{ name: "Justin"}])
+@jenny = User.create([{ name: "Jenny"}])
