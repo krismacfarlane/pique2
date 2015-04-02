5a012c4dad7f0f810efef01f58a804db2d4f7690
Kristen MacFarlane <krismacfarlane@gmail.com>
2015-04-02 13:14:36 -0400

    adds users controller with helper methods

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+end
+
+
+#### DEVISE USER HELPERS #####
+# user_signed_in? -- verifies if a user is signed in
+# current_user -- accesses signed-in user's info
+# user_session -- accesses the scope for the session
