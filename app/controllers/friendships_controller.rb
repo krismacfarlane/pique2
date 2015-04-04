86f66a02e3ff46b27c9e8a7f95a8d944e1a3d4a9
Kamari Aykes <aykeskc@gmail.com>
2015-04-04 17:00:55 -0400

    updates button routes for friend count

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -10,16 +10,33 @@ class FriendshipsController < ApplicationController
   def create
     @friend = Friendship.create
     @friend.save!
+    redirect_to friends_path
   end
 
   def unfriend
-    @unfriend = Friendship.destroy
+    @unfriend = Friendship.destroy(params[:id])
     @unfriend.save
+    redirect_to friend
   end
 
   def show
     @friend = Friendship.find(params[:id])
     @user = User.find(params[:id])
     @friends = Friendship.all
+  end
+
+  def edit
+    @friend = Friendship.find(params[:id])
+  end
+
+  def update
+    friend = Friendship.find(params[:id])
+    friend.update(friend_params)
+    redirect_to friend
+  end
+
+
+  def friend_params
+    params.require(:friend).permit(:popular_model_id, :popular_model_type, :friend_id, :friend_type)
   end
 end
