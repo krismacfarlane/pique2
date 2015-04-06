e17d4551f7596d90deda3c3a7458cf4a30dc7597
Kamari Aykes <aykeskc@gmail.com>
2015-04-06 15:36:29 -0400

    adds extra columns based on most recent wireframes

--- db/migrate/20150402172226_add_columns_to_users.rb
+++ db/migrate/20150402172226_add_columns_to_users.rb
@@ -1,7 +1,9 @@
 class AddColumnsToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :provider, :string
-    add_column :users, :uid, :string
     add_column :users, :projects, :string
+    add_column :users, :about, :string
+    add_column :users, :experience, :string
+    add_column :users, :skills, :string
+    add_column :users, :portfolio, :string
   end
 end
